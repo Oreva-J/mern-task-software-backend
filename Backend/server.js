1f5ee46f95875d755b6162c33dc86df2f7d14a36
Oreva-J <orevaawusi@gmail.com>
2025-01-13 09:14:18 +0100

    vercel added on cors

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-    origin: ['http://localhost:5173/', 'https://mern-task-program.orender.com']
+    origin: ['http://localhost:5173/', 'https://mern-task-program.onrender.com', 'https://mern-task-program.vercel.app/']
 }))
 app.use(taskRoutes)
 
